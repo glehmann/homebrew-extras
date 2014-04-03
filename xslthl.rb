2d7f535d0da0a4d0d53da92f84afe053cf831f5b
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2014-04-03 15:17:21 +0200

    add url

--- xslthl.rb
+++ xslthl.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Xslthl < Formula
-  homepage ""
+  homepage "http://xslthl.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/xslthl/xslthl/2.1.0/xslthl-2.1.0-dist.tar.gz"
   sha1 "77877cc7fa74589a0e24c8151a8cd907378f5307"
 
